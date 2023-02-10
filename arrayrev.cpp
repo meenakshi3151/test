ec1dc2830fcfbb2364ea8ecba20b15d55f18b42e
meenakshi3151 <125037000+meenakshi3151@users.noreply.github.com>
2023-02-10 19:17:55 +0530

    Add files via upload

---
+++ arrayrev.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+int main(){
+    char arr[45]="meenakshi";
+int d=strlen(arr);
+    for(int i=0;i<d;i++){
+        arr[i]=arr[d-1-i];
+ }
+      for(int i=0;i<d;i++){
+        printf("%c",arr[i]);
+      }
+    return 0;
+}
